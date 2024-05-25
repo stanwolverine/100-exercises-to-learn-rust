24bfc0e908874a9b52f7024e821ee48ba5500538
Jatin Nagar <jitu19nagar@gmail.com>
2024-05-25 23:37:31 +0530

    solved 02_basic_calculator/07_for exercise

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,13 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    if n <= 1 {
+        return 1;
+    }
+    let mut result = 1;
+    for num in 1..=n {
+        result *= num;
+    }
+    return result;
 }
 
 #[cfg(test)]
