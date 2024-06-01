ca32a7eeae2e84c00035d5981ea32acaaf1fe012
Jatin Nagar <jitu19nagar@gmail.com>
2024-06-01 11:46:49 +0530

    solved 03_ticket_v1/10_reference_in_memory exercise

--- exercises/03_ticket_v1/10_references_in_memory/src/lib.rs
+++ exercises/03_ticket_v1/10_references_in_memory/src/lib.rs
@@ -13,16 +13,16 @@ mod tests {
 
     #[test]
     fn u16_ref_size() {
-        assert_eq!(size_of::<&u16>(), todo!());
+        assert_eq!(size_of::<&u16>(), 8usize); // size of pointer
     }
 
     #[test]
     fn u64_mut_ref_size() {
-        assert_eq!(size_of::<&mut u64>(), todo!());
+        assert_eq!(size_of::<&mut u64>(), 8usize); // size of pointer
     }
 
     #[test]
     fn ticket_ref_size() {
-        assert_eq!(size_of::<&Ticket>(), todo!());
+        assert_eq!(size_of::<&Ticket>(), 8usize); // size of pointer
     }
 }
